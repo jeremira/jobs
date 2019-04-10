b19b6108297e7eb1e882280e08c67acf4f09b95e
jeremira <jmiraille@gmail.com>
2019-04-10 09:07:24 +0200

    Car full price method

--- backend/level1/car.rb
+++ backend/level1/car.rb
@@ -6,4 +6,8 @@ class Car
     @price_per_km  = params[:price_per_km]
     @price_per_day = params[:price_per_day]
   end
+
+  def full_price_for(days: 0, distance: 0)
+    days * price_per_day + distance * price_per_km
+  end
 end
--- spec/level1/car_spec.rb
+++ spec/level1/car_spec.rb
@@ -41,4 +41,65 @@ RSpec.describe Car do
       end
     end
   end
+
+  describe "#full_price_for" do
+    let(:car_params) do
+       {
+         id: 123,
+         price_per_day: 23,
+         price_per_km: 14
+       }
+    end
+
+    context "with no setup" do
+      let(:on_test) { my_car.full_price_for }
+
+      it "return an Integer" do
+        expect(on_test).to be_an Integer
+      end
+      it "return correct price" do
+        expect(on_test).to eq 0
+      end
+    end
+    context "for one day" do
+      context "but no distance" do
+        let(:on_test) { my_car.full_price_for days: 1 }
+        it "return an Integer" do
+          expect(on_test).to be_an Integer
+        end
+        it "return correct price" do
+          expect(on_test).to eq 23
+        end
+      end
+      context "and some distance" do
+        let(:on_test) { my_car.full_price_for days: 1, distance: 55 }
+        it "return an Integer" do
+          expect(on_test).to be_an Integer
+        end
+        it "return correct price" do
+          expect(on_test).to eq(23 + 55*14)
+        end
+      end
+    end
+    context "for many days" do
+      context "but no distance" do
+        let(:on_test) { my_car.full_price_for days: 6 }
+        it "return an Integer" do
+          expect(on_test).to be_an Integer
+        end
+        it "return correct price" do
+          expect(on_test).to eq 6*23
+        end
+      end
+      context "and some distance" do
+        let(:on_test) { my_car.full_price_for days: 6, distance: 73 }
+        it "return an Integer" do
+          expect(on_test).to be_an Integer
+        end
+        it "return correct price" do
+          expect(on_test).to eq(6*23 + 73*14)
+        end
+      end
+    end
+  end
 end
